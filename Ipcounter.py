387002f9e5b29e9e459f0fb62c500d3dbf34ac61
penguin <penguinnz96@gmail.com>
2020-12-03 17:55:30 +0530

    program hit-counter ready

--- Ipcounter.py
+++ Ipcounter.py
@@ -1,1 +1,21 @@
 import matplotlib.pyplot as plt
+
+
+logfile = 'access.log'
+hit_count = 500
+
+with open (logfile) as logobj:
+
+    hit_counter = {}
+
+    for line in logobj:
+
+        ip = line.split()[0]
+
+        if ip not in hit_counter:
+
+            hit_counter[ip] = 1
+
+        else:
+
+            hit_counter[ip] = hit_counter[ip] + 1
