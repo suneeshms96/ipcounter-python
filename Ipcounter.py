28fdbc5c213040a7fbefc6b6373c62cab54a4406
penguin <penguinnz96@gmail.com>
2020-12-03 18:00:08 +0530

    graph program ready

--- Ipcounter.py
+++ Ipcounter.py
@@ -1,1 +1,16 @@
 import matplotlib.pyplot as plt
+
+for ip in hit_counter:
+
+    hit = hit_counter[ip]
+
+    ips = ip
+
+    hits = hit
+
+    if hit >= hit_count:
+
+        print('{:20} {}'.format(ip,hit))
+
+        plt.bar(ips, hits)
+
